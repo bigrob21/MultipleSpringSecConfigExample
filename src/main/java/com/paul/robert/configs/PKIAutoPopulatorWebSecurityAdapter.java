c30f048bcc703429c307457dd15bdcd9cb5d6a22
bigrob21 <bigrob21@homtail.com>
2018-05-15 23:12:25 -0400

    additional implementation code.

--- src/main/java/com/paul/robert/configs/PKIAutoPopulatorWebSecurityAdapter.java
+++ src/main/java/com/paul/robert/configs/PKIAutoPopulatorWebSecurityAdapter.java
@@ -1,9 +1,45 @@
 package com.paul.robert.configs;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
+import com.paul.robert.security.userdetails.service.AutoPopulatorUserDetailsService;
+
+@Configuration
+@Order(100)
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@Profile({"!dev"})
 public class PKIAutoPopulatorWebSecurityAdapter extends WebSecurityConfigurerAdapter {
 
-	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.anyRequest()
+			.authenticated()
+			.and()
+			.x509()
+			.subjectPrincipalRegex(("(^.*)"))
+			.userDetailsService(userDetailsService())
+			.and()
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.csrf()
+			.disable();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new AutoPopulatorUserDetailsService();
+	}
 	
 }
--- src/main/java/com/paul/robert/web/controllers/WebServiceEndpoint.java
+++ src/main/java/com/paul/robert/web/controllers/WebServiceEndpoint.java
@@ -1,6 +1,7 @@
 package com.paul.robert.web.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class WebServiceEndpoint {
 
 	@GetMapping("/verify")
+	@PreAuthorize("hasAnyAuthority('LVL1','LVL2','LVL3')")
 	public ResponseEntity<String> testVerify(){
 		return ResponseEntity.ok("Hello I'm working!!!!");
 	}
